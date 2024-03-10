76160f80b614d4177f76edf722b2c7cf61d2e617
PenK <spikeb3b@gmail.com>
2024-03-10 11:37:03 -0700

    comment

--- src/Grid.java
+++ src/Grid.java
@@ -13,7 +13,7 @@ public class Grid {
     // private int combo = 0; 
     private int lines;
 
-    // Gravity constants designed to scale up by level https://harddrop.com/wiki/Tetris_Worlds
+    // Gravity constants scale by level https://harddrop.com/wiki/Tetris_Worlds
     private final double[] GRAVITY = new double[] { 0.01667, 0.021017, 0.026977, 0.035256, 0.04693, 0.06361, 0.0879,
             0.1236, 0.1775, 0.2598, 0.388, 0.59, 0.92, 1.46, 2.36 };
     private int dropRate;
